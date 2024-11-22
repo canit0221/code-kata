ef29678d5cbeba14540481c1c0a0e21537853f74
canit0221 <80800758+canit0221@users.noreply.github.com>
2024-11-22 20:13:11 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,18 +1,20 @@
 def solution(numer1, denom1, numer2, denom2):
-
-
+  """
+  두 분수를 더하고 기약분수로 만드는 함수입니다.
+  """
+  # 1. 분수 더하기: 분모는 곱하고, 분자는 분모끼리 곱한 값을 곱해서 더해요.
   top = numer1 * denom2 + numer2 * denom1  # 분자
   bottom = denom1 * denom2  # 분모
 
-
+  # 2. 기약분수 만들기: 최대공약수로 분자와 분모를 나눠요.
   def gcd(a, b):  # 최대공약수 구하는 함수 (유클리드 호제법)
     while b:
       a, b = b, a % b
     return a
 
-  g = gcd(top, bottom) 
-  return [top // g, bottom // g] 
+  g = gcd(top, bottom)  # 분자와 분모의 최대공약수
+  return [top // g, bottom // g]  # 기약분수 (분자, 분모)
 
-
-print(solution(1, 2, 3, 4))
-print(solution(9, 2, 1, 3)) 
+# 예시
+print(solution(1, 2, 3, 4))  # 출력: [5, 4]
+print(solution(9, 2, 1, 3))  # 출력: [29, 6]
