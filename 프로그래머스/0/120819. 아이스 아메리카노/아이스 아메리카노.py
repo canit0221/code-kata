cf6946599404835237c027d335fc3d57ad05d656
canit0221 <80800758+canit0221@users.noreply.github.com>
2024-11-26 11:20:59 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,11 +1,12 @@
 def solution(money):
     answer = [0,0]
     
-    answer[0] = money // 5500
+    answer[0] = money // 5500 # 커피가 몇잔?
     
-    if money >= 5500 :
+    if money >= 5500 : #한잔이상 사먹을때 잔돈
         answer[1] = money % 5500
-    else:
-        answer[1] = money
+        
+    else: #한잔도 못마실경우 잔돈
+        answer[1] = money 
     
     return answer
