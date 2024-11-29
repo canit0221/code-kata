b10e425e64901a4958c5b9a8de106e9098c10f7b
canit0221 <80800758+canit0221@users.noreply.github.com>
2024-11-29 19:06:29 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,8 +1,6 @@
+import numpy as np
 def solution(num_list, n):
-    answer = []
-    array=list(range(0,len(num_list),n))
+    answer = [[]]
+    num_list.reshape(n,num_list/n)
     
-    for i in array:
-        answer.append(num_list[i:n+i])
-
     return answer
