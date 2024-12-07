14480462f60b56a52d12b86fb8d787b77c067b6f
canit0221 <80800758+canit0221@users.noreply.github.com>
2024-12-07 17:27:12 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,13 @@
+def solution(my_string):
+    answer = ''
+    my_string = list(my_string)
+    remove = ['a','e','i','o','u']
+    
+    my_string = [i for i in my_string if i not in remove]
+
+            
+   
+    answer = ''.join(my_string)
+    
+    
+    return answer
