825ba3e70d47ecc749312b424aac8cf29a8ca23f
canit0221 <80800758+canit0221@users.noreply.github.com>
2024-12-16 17:41:21 +0900

    [level 0] Title: 옹알이 (1), Time: 0.24 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -3,15 +3,15 @@ def solution(babbling):
     pron = ["aya", "ye", "woo", "ma"]
     
     for b in babbling:
-        # 발음이 남아있을 동안 반복
+        
         while True:
             original_b = b  # 원래 문자열 저장
             for p in pron:
-                if b.find(p) == 0:
-                    b = b.replace(p, '', 1)  # 첫 번째 발생만 제거
+                if b.find(p) == 0: # 첫번째에 있을 때만 제거
+                    b = b.replace(p, '')
             if original_b == b:  # 더 이상 변경이 없으면 종료
                 break
-        if len(b) == 0:
+        if len(b)==0:
             answer += 1
 
     return answer
