06c6cdba03bf684374d7fab2f94d69fc8fefac9c
canit0221 <80800758+canit0221@users.noreply.github.com>
2024-12-17 09:58:33 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,13 +1,21 @@
 def solution(array):
+    # 각 숫자의 출현 횟수를 저장할 딕셔너리 초기화
     counts = {}
+    
+    # 배열의 고유한 값들에 대해 반복
     for i in set(array):
+        # 각 숫자의 출현 횟수를 계산하여 counts 딕셔너리에 저장
         counts[i] = array.count(i)
     
-   
+    # 가장 높은 출현 횟수를 찾음
     max_count = max(counts.values())
+    
+    # 가장 높은 출현 횟수를 가진 숫자들을 리스트로 저장
     modes = [key for key, value in counts.items() if value == max_count]
     
+    # 최빈값이 여러 개일 경우 -1을 반환
     if len(modes) > 1:
         return -1
     else:
-        return modes[0] 
+        # 최빈값이 하나일 경우 그 값을 반환
+        return modes[0]
