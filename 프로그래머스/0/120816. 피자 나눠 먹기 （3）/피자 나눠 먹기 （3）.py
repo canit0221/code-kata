af779b14f55ff50c0ac5a94f2797d7cae7628de3
canit0221 <80800758+canit0221@users.noreply.github.com>
2024-12-18 10:56:51 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,10 +1,7 @@
 def solution(slice, n):
-    answer = 0
     pizza = 1
     
     while (pizza * slice) // n < 1:
         pizza += 1
     
-    answer = pizza
-    
-    return answer
+    return pizza
