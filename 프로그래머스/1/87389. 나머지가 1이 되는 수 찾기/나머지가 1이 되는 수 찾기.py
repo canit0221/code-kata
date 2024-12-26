f021f851cdea51dca3f4e9d20b96bfc975675030
canit0221 <80800758+canit0221@users.noreply.github.com>
2024-12-26 10:09:06 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 87.22 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
@@ -1,9 +1,9 @@
 def solution(n):
     answer = 0
-    divide = []
     
-    for i in range(1,n):
+    for i in range(n,1,-1):
         if n % i == 1:
             answer = i
-            break
+            
+            
     return answer
