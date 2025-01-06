78e015724c39d3586cb171abc7e8eaa7857d71db
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-01-06 12:02:57 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,17 +1,16 @@
 def solution(numbers):
-    diction = {
-        "zero" : "0", "one" : "1", "two" : "2",
-        "three" : "3", "four" : "4", "five" : "5",
-        "six" : "6", "seven" : "7", "eight" : "8",
-        "nine" : "9"
-    };
+    number_alpha = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    number_digit = list(range(0,10))
+    
+    diction = dict(zip(number_alpha,number_digit))
+    
     string = ''
     answer = ''
     for i in numbers:
         string += i
         
         if string in diction:
-            answer += diction[string]
+            answer += str(diction[string])
             string = ''
     answer = int(answer)
         
