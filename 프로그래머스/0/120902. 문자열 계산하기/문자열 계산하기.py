9acd5ba52bd4c33fc767dd87b72a3703616a6d58
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-01-06 11:29:56 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -0,0 +1,18 @@
+def solution(my_string):
+    
+    cal_list = my_string.split()
+    
+    answer = int(cal_list[0])
+    i=1
+    
+    while len(cal_list) > i:
+        
+        if cal_list[i] =='+':
+            answer += int(cal_list[i+1])
+        else:
+            answer -= int(cal_list[i+1])
+            
+        i+=2
+              
+    print(cal_list)
+    return answer
