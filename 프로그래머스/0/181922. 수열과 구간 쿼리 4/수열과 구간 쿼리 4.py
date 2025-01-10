22e87e9b6cac7abeb139fe02424ac97e56d75f46
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-01-10 09:16:59 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 37.80 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -0,0 +1,10 @@
+def solution(arr, queries):
+    
+    for i in range(0,len(arr)):
+        
+        for s,e,k in queries:
+            
+            if i >= s and i <= e and i%k==0:
+                arr[i] += 1
+                
+    return arr
