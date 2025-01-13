79069edaf464b3fbf1e4ebe22390c6afd3dd395e
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-01-13 09:17:25 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.py
@@ -1,11 +1,4 @@
 def solution(array):
-    answer = 0
-    array = map(str,array)
-    array = "".join(array)
-    
-    for i in array:
-        if i == "7":
-            answer += 1
-    
+    answer = str(array).count("7")
     
     return answer
