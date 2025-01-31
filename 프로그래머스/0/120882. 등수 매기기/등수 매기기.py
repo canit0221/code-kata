dff1a5ace3ec74692d4825467a0fe63c32edb993
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-01-31 10:11:44 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,10 +1,12 @@
 def solution(score):
-    avg_list = []
+    avg_list = [] # 반복문 전에 리스트 초기화
     
     for i in score:
+		    # 평균 계산
         avg_list.append(sum(i)/2)
-        
+    # 역순으로 정렬된 리스트 만들기    
     sorted_avg = sorted(avg_list,reverse=1)
+    # 정렬된 리스트의 인덱스를 참조하여 순위 리스트 작성
     rank = [sorted_avg.index(avg)+1 for avg in avg_list]
 
     return rank
