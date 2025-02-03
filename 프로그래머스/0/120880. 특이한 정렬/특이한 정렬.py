2f2ef68ffb288f8fa3fe9f65a43696a8e0c8a599
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-02-04 01:13:27 +0900

    [level 0] Title: 특이한 정렬, Time: 0.64 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,4 +1,5 @@
 def solution(numlist, n):
+    numlist.sort(reverse=True)
     # 정렬이 완료될때까지 반복
     while True:
         # 변화가 있는지 확인하기 위해 이전의 numlist를 저장
@@ -8,10 +9,6 @@ def solution(numlist, n):
             # 거리가 가까운걸 앞으로 배치
             if abs(numlist[i-1]-n)>abs(numlist[i]-n):
                 numlist[i-1],numlist[i] = numlist[i],numlist[i-1]
-            # 거리가 동일하다면 더 큰수를 앞으로 배치    
-            elif abs(numlist[i-1]-n)==abs(numlist[i]-n):
-                minmax=[numlist[i-1],numlist[i]]
-                numlist[i-1],numlist[i] =max(minmax),min(minmax)
         # 변화가 없다면 정렬된 리스트 이므로 반환   
         if tmp == numlist:
             return numlist
