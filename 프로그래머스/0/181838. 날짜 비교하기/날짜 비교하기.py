1f63d106bec6c385e0ffec792b353265e2db42ca
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-02-10 09:44:07 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,16 +1,2 @@
 def solution(date1, date2):
-
-    if date1[0] < date2[0]: # 연도가 빠름
-        return 1
-        
-    elif date1[0] == date2[0]: # 연도가 같음
-        
-        if date1[1] < date2[1]: # 월이 빠름
-            return 1
-            
-        elif date1[1] == date2[1]: #월이 같음
-            
-            if date1[2] < date2[2]: # 날짜가 빠름
-                return 1
-                
-    return 0
+    return 1 if date1<date2 else 0
