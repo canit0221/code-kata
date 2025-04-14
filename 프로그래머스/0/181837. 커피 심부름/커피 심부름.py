c5a0fe21384e41b805fc1ef28cfa20e489d2a5aa
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-04-14 17:31:32 +0900

    [level 0] Title: 커피 심부름, Time: 0.14 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,9 @@
+def solution(order):
+    answer = 0
+    for i in order:
+        if "latte" in i:
+            answer += 5000
+        else:
+            answer += 4500
+        
+    return answer
