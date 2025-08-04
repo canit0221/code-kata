1feec437ae85b1379e3d1a3b72b7a889d56fc2c1
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-08-04 10:49:06 +0900

    [Bronze II] Title: 방 배정하기, Time: 204 ms, Memory: 17664 KB, Score: 5 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.java
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int A = sc.nextInt();
+		int A = 1;
 		int B = sc.nextInt();
 		int C = sc.nextInt();
 		int N = sc.nextInt();
