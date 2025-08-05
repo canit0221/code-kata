97a5848d3345e65c97a9c6e460eacbb050eda7e3
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-08-05 13:05:58 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 356 ms, Memory: 107,560 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
@@ -13,48 +14,38 @@ public class Solution {
 			for (int i = 0; i < 100; i++) {
 				for (int j = 0; j < 100; j++) {
 					arr[i][j] = sc.nextInt();
-
 				}
 			}
 
 			int max = 0;
-			int sum;
+			int sumRow;
+			int sumCol;
+			int sumL;
+			int sumR;
+			int sumMax;
+
 			// 각 행의 합
 			for (int i = 0; i < 100; i++) {
-				sum = 0;
+				int[] sum = new int[4];
+				sumRow = 0;
+				sumCol = 0;
+				sumL = 0;
+				sumR = 0;
 				for (int j = 0; j < 100; j++) {
-					sum += arr[i][j];
+					sum[0] += arr[i][j];
+					sum[1] += arr[j][i];
+					sum[2] += arr[j][j];
+					sum[3] += arr[j][100 - j - 1];
 				}
-				max = Math.max(max, sum);
+				Arrays.sort(sum);
+				sumMax = sum[3];
+				max = Math.max(sumMax, max);
 			}
 
-			// 각 열의 합
-			for (int i = 0; i < 100; i++) {
-				sum = 0;
-				for (int j = 0; j < 100; j++) {
-					sum += arr[j][i];
-				}
-				max = Math.max(max, sum);
-			}
-
-			// 각 대각선의 합
-			sum = 0;
-			for (int i = 0; i < 100; i++) {
-				sum += arr[i][i];
-			}
-			max = Math.max(max, sum);
-
-			// 각 대각선의 합
-			sum = 0;
-			for (int i = 0; i < 100; i++) {
-				sum += arr[i][100-i-1];
-			}
-			max = Math.max(max, sum);
-			
-			System.out.printf("#%d %d",tc,max);
+			System.out.printf("#%d %d", tc, max);
 			System.out.println();
-			
-		}//테스트케이스
+
+		} // 테스트케이스
 
 	}
 }
