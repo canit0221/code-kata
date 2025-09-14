e5d11d442d294a4af46280d49bfac3484ba5f09a
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-09-15 00:52:21 +0900

    [Silver I] Title: 미로 탐색, Time: 212 ms, Memory: 19104 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,61 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	static int N, M;
+	static int[][] map;
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		M = sc.nextInt();
+		map = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
+			String line = sc.next();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = line.charAt(j)-'0';
+			}
+		} // 입력 받기
+
+		System.out.println(bfs(0,0));
+		
+	}// main
+
+	static int bfs(int i, int j){
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {i,j,1});
+		map[i][j] = 0;
+		
+		while(!q.isEmpty()) {
+			int[] curr = q.poll();
+			if  (curr[0] == N-1 && curr[1] == M-1)
+				return curr[2];
+			
+			for(int k=0;k<4;k++) {
+				int nr = curr[0] + dr[k];
+				int nc = curr[1] + dc[k];
+				
+				if(nr<0||nc<0||nr>=N||nc>=M)
+					continue;
+				
+				if(map[nr][nc]!=0) {
+					map[nr][nc] = 0;
+					q.add(new int[] {nr,nc,curr[2]+1});
+				}
+			}
+			
+			
+			
+		}
+		
+		return -1;
+		
+		
+	}
+}
