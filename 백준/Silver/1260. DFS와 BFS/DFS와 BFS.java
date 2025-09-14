8b003ac9808cf8f3300f97438d5bc376a0915c94
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-09-15 00:05:05 +0900

    [Silver II] Title: DFS와 BFS, Time: 496 ms, Memory: 38692 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -6,10 +6,10 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-
-	static int V, E, start;
-	static ArrayList<Integer>[] adjList;
+	static int V, E;
+	static ArrayList<Integer>[] A;
 	static boolean[] visited;
+	static int start;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -18,58 +18,60 @@ public class Main {
 		E = sc.nextInt();
 		start = sc.nextInt();
 
-		adjList = new ArrayList[V + 1];
+		A = new ArrayList[V + 1];
 		for (int i = 1; i < V + 1; i++) {
-			adjList[i] = new ArrayList<>();
-		} // 초기화
-
-		for (int i = 0; i < E; i++) {
+			A[i] = new ArrayList<>();
+		}// 인접리스트 초기화
+		
+		for(int i = 0; i<E; i++) {
 			int s = sc.nextInt();
 			int e = sc.nextInt();
-
-			adjList[s].add(e);
-			adjList[e].add(s);
+			
+			A[s].add(e);
+			A[e].add(s);
+		}//입력
+		
+		for(int i = 1; i<V+1; i++) {
+			Collections.sort(A[i]);
 		}
-
-		for (int i = 1; i < V + 1; i++) {
-			Collections.sort(adjList[i]);
-		}
-
-		visited = new boolean[V + 1];
+		
+		visited = new boolean[V+1];
 		dfs(start);
 		System.out.println();
-		visited = new boolean[V + 1];
+		
+		visited = new boolean[V+1];
 		bfs(start);
 		System.out.println();
-
-	}
-
+		
+	}//main
+	
 	static void dfs(int v) {
-		System.out.print(v + " ");
 		visited[v] = true;
-
-		for (int w : adjList[v]) {
-			if (!visited[w]) {
+		System.out.print(v+" ");
+		
+		for(int w:A[v]) {
+			if(!visited[w]) {
 				dfs(w);
 			}
 		}
-	}
-
+	}//dfs
+	
 	static void bfs(int v) {
-		Queue<Integer> q = new LinkedList<Integer>();
+		Queue<Integer> q = new LinkedList<>();
 		q.add(v);
 		visited[v] = true;
-
-		while (!q.isEmpty()) {
+		
+		while(!q.isEmpty()) {
 			int curr = q.poll();
-			System.out.print(curr + " ");
-			for (int w : adjList[curr]) {
-				if (!visited[w]) {
-					visited[w] = true;
+			System.out.print(curr +" ");
+			
+			for(int w:A[curr]) {
+				if(!visited[w]) {
 					q.add(w);
+					visited[w] = true;
 				}
 			}
 		}
-	}
-
+		
+	}//bfs
 }
