4d8af00913446a43ea8c2d17d584ca188da3bdbe
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-09-18 15:31:04 +0900

    [Silver II] Title: 마인크래프트, Time: 328 ms, Memory: 33284 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -1,89 +1,66 @@
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int oB;
-	static int N, M, cnt, min, max, height;
-	static int[] h = new int[257];
-	static int[] map;
-	static boolean isOk;
+    static int oB;
+    static int N, M, cnt, min = 256, max = 0, height;
+    static int[] h = new int[257];
+    static boolean isOk;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        oB = Integer.parseInt(st.nextToken());
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		oB = Integer.parseInt(st.nextToken());
+        // 입력: 카운팅 + min/max 갱신 (정렬/배열 불필요)
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                int v = Integer.parseInt(st.nextToken());
+                h[v]++;
+                if (v < min) min = v;
+                if (v > max) max = v;
+            }
+        }
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				if (!st.hasMoreTokens())
-					st = new StringTokenizer(br.readLine());
-				int v = Integer.parseInt(st.nextToken());
-				h[v]++;
-				if (v < min)
-					min = v;
-				if (v > max)
-					max = v;
-			}
-		} // 입력
+        height = 0;
+        int minCnt = Integer.MAX_VALUE;
 
-		// 1. map[i][j]-- -> B++, cnt+=2
-		// 2. B-- -> map[i][j]++, cnt++
-		// 0 <= map[i][j] <=256
+        for (int target = min; target <= max; target++) {
+            isOk = true;
+            int B = oB;
+            cnt = 0;
 
-		// 채울수 있는게 채우는게 좋음
-		// 채우는거 두번 = 제거하는것
+            // 1) 제거 먼저 (인벤 채우기)
+            for (int i = 0; i <= 256; i++) {
+                int c = h[i];
+                if (c == 0 || i <= target) continue;
+                int diff = (i - target) * c;
+                cnt += diff * 2; // 제거 시간
+                B   += diff;     // 인벤 증가
+            }
 
-		// 어디로 모일까? -> 평균, 최빈값,
-		// 못정할거같음
+            // 2) 그 다음 채우기 (인벤 확인)
+            for (int i = 0; i <= 256; i++) {
+                int c = h[i];
+                if (c == 0 || i >= target) continue;
+                int need = (target - i) * c;
+                if (need > B) { isOk = false; break; }
+                cnt += need; // 채우기 시간
+                B   -= need; // 인벤 감소
+            }
 
-		// 그럼 어차피 최소값과 최대값 사이에서 수렴하는 값이 결정되고
-		// 정답 값을 목표로해서 완전탐색을 해보자
+            // 최소 시간, 동률이면 더 높은 높이
+            if (isOk && (cnt < minCnt || (cnt == minCnt && target > height))) {
+                minCnt = cnt;
+                height = target;
+            }
+        }
 
-		height = 0;
-		int minCnt = Integer.MAX_VALUE;
-		for (int target = min; target <= max; target++) {
-			isOk = true;
-			int B = oB;
-			cnt = 0;
-
-			for (int i = 0; i <= 256; i++) {
-				if (h[i] > 0) {
-					if (i > target) { // 제거
-						cnt += (i - target) * 2 * h[i];
-						B += (i - target) * h[i];
-					}
-
-				}
-			}
-			for (int i = 0; i <= 256; i++) {
-				if (h[i] > 0) {
-					if (i < target) {
-						if ((target - i) * h[i] > B) {
-							isOk = false;
-							break;
-						}
-						cnt += (target - i) * h[i]; // 쌓기
-						B -= (target - i) * h[i];
-					}
-				}
-			}
-
-			if (isOk) {
-				minCnt = Math.min(minCnt, cnt);
-				if (minCnt == cnt) {
-					height = target;
-				}
-			}
-
-		}
-
-		System.out.println(minCnt + " " + height);
-	}// main
+        System.out.println(minCnt + " " + height);
+    }
 }
