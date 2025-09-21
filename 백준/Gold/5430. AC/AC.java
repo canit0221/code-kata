19eb1cfe6088136d48a65c9ca5d8364da1abed9c
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-09-21 16:26:34 +0900

    [Gold V] Title: AC, Time: 1780 ms, Memory: 105932 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.java
@@ -0,0 +1,87 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	static List<String> numList;
+	static boolean reverse;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+
+		for (int tc = 0; tc < T; tc++) {
+
+			String p = sc.next();
+			int N = sc.nextInt();
+			sc.nextLine();
+			// [ 다음 숫자
+			// , 다음 숫자
+			// ] 나오면 종료
+			String str = sc.nextLine();
+			boolean error = false;
+			boolean blank = false;
+			numList = new ArrayList<>();
+			
+			if (N > 0) // 배열 비었음
+			{
+				str = str.substring(1, str.length() - 1);
+				numList = new ArrayList<>(Arrays.asList(str.split(",")));
+			}
+			reverse = false;
+
+			for (int i = 0; i < p.length(); i++) {
+				if (p.charAt(i) == 'R') {
+					if (reverse) {
+						reverse = false;
+					} else {
+						reverse = true;
+					}
+				} // 뒤집기
+				else { // 제거
+					if (numList.isEmpty()) { // 뺄수 없다.
+						error = true;
+						break;
+					}
+
+					if (reverse) {
+						numList.remove(numList.size() - 1);
+					} else {
+						numList.remove(0);
+					}
+				}
+			}
+
+			StringBuilder sb = new StringBuilder();
+
+			if (error)
+				sb.append("error");
+
+			else {
+				sb.append('[');
+				if (reverse) {
+					for (int i = numList.size() - 1; i >= 0; i--) {
+
+						sb.append(numList.get(i)).append(',');
+					}
+				}
+
+				else {
+					for (int i = 0; i < numList.size(); i++) {
+
+						sb.append(numList.get(i)).append(',');
+					}
+				}
+				if(sb.length()>1)
+					sb.deleteCharAt(sb.length() - 1);
+				sb.append(']');
+			}
+
+			System.out.println(sb);
+
+		}
+	}
+}
