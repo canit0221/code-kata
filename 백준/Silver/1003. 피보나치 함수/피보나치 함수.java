fcd12c823f9a08e1a3abcf5c35e8a7ea6cbae859
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-09-29 02:08:46 +0900

    [Silver III] Title: 피보나치 함수, Time: 196 ms, Memory: 18516 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] zero = new int[41];
+		int[] one = new int[41];
+
+		zero[0] = 1;
+		one[0] = 0;
+		zero[1] = 0;
+		one[1] = 1;
+
+		for (int i = 2; i <= 40; i++) {
+			zero[i] = zero[i - 1] + zero[i - 2];
+			one[i] = one[i - 1] + one[i - 2];
+		}
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+			System.out.println(zero[N]+" "+one[N]);
+		}
+	}
+}
