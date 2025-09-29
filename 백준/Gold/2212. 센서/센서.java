a7cce0177e893179c7546cff41d8abbe874b260e
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-09-29 21:52:35 +0900

    [Gold V] Title: 센서, Time: 356 ms, Memory: 29812 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] x = new int[N];
+        for (int i = 0; i < N; i++) x[i] = sc.nextInt();
+
+        if (K >= N) {
+            System.out.println(0);
+            return;
+        }
+
+        Arrays.sort(x);
+
+        
+        int[] d = new int[N - 1]; // 간격 배열
+        for (int i = 0; i < N - 1; i++) d[i] = x[i + 1] - x[i];
+        
+        // 간격을 오름차순 정렬 → 작은 간격부터 N-K개 더하면 최소합
+        // K개로 나눈다 -> K-1번 자른다 -> N-1번에서 큰 간격 K-1번을 뺸다
+        // -> N-1 -(K-1) => 작은 간격 N-K개가 남는다.
+        Arrays.sort(d);
+
+        int ans = 0; 
+        for (int i = 0; i < N-K; i++) ans += d[i];
+
+        System.out.println(ans);
+    }
+}
