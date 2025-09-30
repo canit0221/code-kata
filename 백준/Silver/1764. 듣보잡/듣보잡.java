7fb5b118b137b31adb59eb120a0804c790cc6e82
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-09-30 14:28:08 +0900

    [Silver IV] Title: 듣보잡, Time: 724 ms, Memory: 72632 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,42 @@
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+	static int N, M;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		M = sc.nextInt();
+		Set<String> hear = new HashSet<>();
+		Set<String> listen = new HashSet<>();
+		for (int i = 0; i < N; i++) {
+			hear.add(sc.next());
+		}
+
+		for (int i = 0; i < M; i++) {
+			listen.add(sc.next());
+		}
+		List<String> ans = new ArrayList<>();
+		
+		for (String x : hear) {
+			if (listen.contains(x)) {
+				ans.add(x);
+			}
+		}
+		
+		Collections.sort(ans);
+		
+		System.out.println(ans.size());
+		for(int i = 0; i<ans.size(); i++) {
+			System.out.println(ans.get(i));
+		}
+	}
+}
