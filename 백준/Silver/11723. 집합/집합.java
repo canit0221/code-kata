72d42b432287b326683f63951080abc8ff299a52
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-09-30 14:08:45 +0900

    [Silver V] Title: 집합, Time: 3832 ms, Memory: 329600 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.java
@@ -0,0 +1,57 @@
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+	static HashSet<Integer> set = new HashSet<>();
+	static HashSet<Integer> all = new HashSet<>();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		for (int i = 1; i < 21; i++) {
+			all.add(i);
+		}
+		StringBuilder sb = new StringBuilder();
+		int M = sc.nextInt();
+		for (int m = 0; m < M; m++) {
+
+			String command = sc.next();
+			int x = 0;
+			if (!"allempty".contains(command)) {
+				x = sc.nextInt();
+			}
+
+			switch (command) {
+			case "add":
+				set.add(x);
+				break;
+			case "remove":
+				set.remove(x);
+				break;
+			case "check":
+				if (set.contains(x))
+					sb.append(1);
+				else
+					sb.append(0);
+				sb.append('\n');
+				break;
+			case "toggle":
+				if (set.contains(x))
+					set.remove(x);
+				else
+					set.add(x);
+				break;
+			case "all":
+				set.clear();
+				set.addAll(all);
+				break;
+			case "empty":
+				set.clear();
+				break;
+
+			}
+		}
+		System.out.println(sb.toString());
+	}
+}
