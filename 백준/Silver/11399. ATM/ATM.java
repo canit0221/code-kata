64fa57d04f09dc98c1195cf94fd80e6486df3d3d
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-09-30 15:53:57 +0900

    [Silver IV] Title: ATM, Time: 236 ms, Memory: 20968 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,26 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int[] p = new int[N];
+		for (int i = 0; i < N; i++) {
+			p[i] = sc.nextInt();
+		}
+
+		Arrays.sort(p);
+
+		int ans = 0;
+		for (int i = 0; i < N; i++) {
+			for(int j= 0; j<=i; j++) {
+				ans += p[j];
+			}
+		}
+		
+		System.out.println(ans);
+	}
+}
