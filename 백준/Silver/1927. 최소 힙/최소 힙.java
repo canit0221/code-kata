fb094ab207e0bbe77aa0521c838e24762c03f3df
canit0221 <80800758+canit0221@users.noreply.github.com>
2025-10-02 14:56:09 +0900

    [Silver II] Title: 최소 힙, Time: 864 ms, Memory: 113096 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,34 @@
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N  = sc.nextInt();
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i<N; i++) {
+			int num = sc.nextInt();
+			if(num !=0 )
+				pq.add(num);
+			else {
+				if(pq.isEmpty())
+					sb.append(0).append('\n');
+				else {
+					sb.append(pq.poll()).append('\n');
+				}
+					
+			}
+				
+		}
+		
+		System.out.println(sb.toString());
+		
+		
+		
+		
+	}
+}
